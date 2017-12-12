59a2cdcdd4115d45bbe6f05a13d60935ab5f1b30
Uyen Truong <uyen@Uyens-MacBook-Air.local>
2017-12-11 16:14:55 -0800

    benchmark models

--- __init__.py
+++ __init__.py
@@ -3,6 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, HashingVectorizer
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.model_selection import GridSearchCV, train_test_split, StratifiedShuffleSplit
+from sklearn.preprocessing import Imputer
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import log_loss,roc_auc_score, roc_curve, accuracy_score, confusion_matrix, classification_report
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
 
 
 targets = ['Function', 'Use', 'Sharing', 'Reporting', 'Student_Type',
